2cb861b3b89d81b9314161a9a68216d66ef3c38e
RuipengWang <rpwang@Edwards-MacBook-Pro.local>
2018-03-31 02:06:32 +0800

    Change to use zeppelin-solidity mineable token -- Ruipeng Wang

--- contracts/P2B.sol
+++ contracts/P2B.sol
@@ -1,54 +1,15 @@
 pragma solidity ^0.4.16;
 
-contract TokenERC20 {
-    address public owner;
-    string public name;
-    string public symbol;
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
+import "node_modules/zeppelin-solidity/contracts/token/ERC20/";
 
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+contract P2B is MintableToken {
+    string public name = "P2 token";
+    string public symbol = "P2B";
+    uint public decimals = 18;
+    uint public INITIAL_SUPPLY = 12000;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function TokenERC20(uint256 initialSupply, string tokenName, string tokenSymbol) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
-        name = tokenName;
-        symbol = tokenSymbol;
-        owner = msg.sender;
-    }
-    
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        Transfer(_from, _to, _value);
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
-
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);
-    }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-
-    function mint(address targetAddress, uint256 mintedAmount) onlyOwner public {
-        balanceOf[targetAddress] += mintedAmount;
-        totalSupply += mintedAmount;
-        Transfer(0, owner, mintedAmount);
-        Transfer(owner, targetAddress, mintedAmount);
+    function P2B() {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
     }
 }
